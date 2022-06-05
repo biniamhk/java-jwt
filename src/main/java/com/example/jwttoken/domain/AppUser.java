20a13847df4b44bb477cd10d2fb2d425140dec9c
biniam haile <biniamhk@gmail.com>
2022-06-05 11:52:33 +0200

    methode added to repo

--- src/main/java/com/example/jwttoken/domain/AppUser.java
+++ src/main/java/com/example/jwttoken/domain/AppUser.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import static javax.persistence.FetchType.EAGER;
 
 @Entity @Data @NoArgsConstructor @AllArgsConstructor
-public class AppUser {
+public class  AppUser {
     @Id @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String name;
--- src/main/java/com/example/jwttoken/repository/RoleRepo.java
+++ src/main/java/com/example/jwttoken/repository/RoleRepo.java
@@ -3,7 +3,10 @@ package com.example.jwttoken.repository;
 import com.example.jwttoken.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
     Role findByName(String name);
+    Optional<Role> findById(Long id);
 }
